9f70f237bb65fe335019352ffa41adfcce4fb884
gonzalofdv <gfigue01@ucm.es>
2020-05-30 11:17:27 +0200

    Update Primera Iteracion Reglas.R

--- Modelos/Primera Iteracion Reglas.R
+++ Modelos/Primera Iteracion Reglas.R
@@ -1,7 +1,21 @@
+library("RMySQL")
+
+set.seed(28768)
+
+con <- dbConnect(RMySQL::MySQL(),
+                 host="localhost",
+                 dbname = "mineriabd",
+                 user = "root",
+                 password = "")
+
+dataRules<-dbGetQuery(con, "SELECT * FROM cancion")
+
+
 if (!require(arules)) {
   install.packages("arules",
                    dependencies = TRUE)}
 require(arules)
+
 rules.all <- apriori(dataRules)
 dataRules$track_id <- NULL
 rules.all <- apriori(dataRules)
